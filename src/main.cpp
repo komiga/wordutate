d3f45fdc48cd008efd5755c148a81e5e60d2ac73
Coranna Howard <me@komiga.com>
2013-12-17 23:41:23 -0500

    main: diagnostic-ignored -Wold-style-cast; removed broken [[noreturn]].¹
    1: Clang 3.5 claims handle_interrupt() is a type (even if the
    declaration were removed and [[noreturn] was moved to the definition),
    and my GCC (4.7.3) has no clue what I'm talking about.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "./InputHandler.hpp"
 #include "./Word.hpp"
 
+#include <duct/config.hpp>
 #include <duct/debug.hpp>
+
 #include <csignal>
 #include <cstdio>
 #include <cstdlib>
@@ -17,15 +19,29 @@ shutdown() __attribute__((destructor (999)));
 static void
 handle_interrupt(
 	signed
-) [[noreturn]];
+) /*[[noreturn]]*/;
 
 signed
 main();
 
+#if (DUCT_COMPILER & DUCT_FLAG_COMPILER_GCC) ||			\
+	((DUCT_COMPILER & DUCT_FLAG_COMPILER_CLANG) &&		\
+	  DUCT_COMPILER > DUCT_FLAG_COMPILER_CLANG33)
+//
+	#define WORDUTATE_IGNORE_OLD_STYLE_CASTS
+#endif
+
 static void
 init() {
 	DUCT_DEBUG_CALLED();
+	#ifdef WORDUTATE_IGNORE_OLD_STYLE_CASTS
+		#pragma GCC diagnostic push
+		#pragma GCC diagnostic ignored "-Wold-style-cast"
+	#endif
 	std::signal(SIGINT, handle_interrupt);
+	#ifdef WORDUTATE_IGNORE_OLD_STYLE_CASTS
+		#pragma GCC diagnostic pop
+	#endif
 }
 
 static void
@@ -35,7 +51,14 @@ shutdown() {
 
 static void
 handle_interrupt(signed) {
+	#ifdef WORDUTATE_IGNORE_OLD_STYLE_CASTS
+		#pragma GCC diagnostic push
+		#pragma GCC diagnostic ignored "-Wold-style-cast"
+	#endif
 	std::signal(SIGINT, SIG_DFL);
+	#ifdef WORDUTATE_IGNORE_OLD_STYLE_CASTS
+		#pragma GCC diagnostic pop
+	#endif
 	std::exit(0);
 }
 
